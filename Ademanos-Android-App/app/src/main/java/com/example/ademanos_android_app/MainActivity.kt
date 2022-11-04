2f5183bf3eb8915707fffbf428c0ece9017ca946
Miguel Arriaga Velasco <marriagav3@gmail.com>
2022-11-04 15:21:23 -0600

    quick nit file managment

--- Ademanos-Android-App/app/src/main/java/com/example/ademanos_android_app/MainActivity.kt
+++ Ademanos-Android-App/app/src/main/java/com/example/ademanos_android_app/MainActivity.kt
@@ -8,8 +8,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import com.example.ademanos_android_app.levelTab.LevelManager
 import com.example.ademanos_android_app.ui.theme.AdemanosAndroidAppTheme
 
@@ -45,37 +43,3 @@ data class BottomNavScreen(
     @DrawableRes val icon: Int,
     val component: @Composable () -> Unit
 )
-
-@Composable
-fun BottomNavigation(
-    screens: List<BottomNavScreen>,
-    modifier: Modifier = Modifier
-) {
-    var selectedScreen by remember { mutableStateOf(0) }
-    Column(modifier = modifier) {
-        Surface(
-            modifier= Modifier
-        ) {
-            screens[selectedScreen].component()
-        }
-        NavigationBar(
-            modifier=Modifier
-        ){
-            for (i in screens.indices) {
-                NavigationBarItem(
-                    icon = {
-                        Icon(
-                            painter = painterResource(screens[i].icon),
-                            tint = MaterialTheme.colorScheme.onPrimaryContainer,
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(30.dp)
-                        )
-                    },
-                    onClick = { selectedScreen = i },
-                    selected = selectedScreen==i
-                )
-            }
-        }
-    }
-}
