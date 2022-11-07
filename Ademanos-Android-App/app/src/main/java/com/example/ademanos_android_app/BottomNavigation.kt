9dde0395ece88ea404a897362989315dc27f7744
Jacobo Soffer <sofferjacob@gmail.com>
2022-11-07 01:33:47 -0600

    mucho

--- Ademanos-Android-App/app/src/main/java/com/example/ademanos_android_app/BottomNavigation.kt
+++ Ademanos-Android-App/app/src/main/java/com/example/ademanos_android_app/BottomNavigation.kt
@@ -1,6 +1,5 @@
 package com.example.ademanos_android_app
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -11,33 +10,27 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun BottomNavigation(
     screens: List<BottomNavScreen>,
+    onSelected: (i: Int) -> Unit,
+    selected: Int,
     modifier: Modifier = Modifier
 ) {
-    var selectedScreen by remember { mutableStateOf(0) }
-    Column(modifier = modifier) {
-        Surface(
-            modifier= Modifier
-        ) {
-            screens[selectedScreen].component()
-        }
-        NavigationBar(
-            modifier= Modifier
-        ){
-            for (i in screens.indices) {
-                NavigationBarItem(
-                    icon = {
-                        Icon(
-                            painter = painterResource(screens[i].icon),
-                            tint = MaterialTheme.colorScheme.onPrimaryContainer,
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(30.dp)
-                        )
-                    },
-                    onClick = { selectedScreen = i },
-                    selected = selectedScreen==i
-                )
-            }
+    NavigationBar(
+        modifier = Modifier
+    ) {
+        for (i in screens.indices) {
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        painter = painterResource(screens[i].icon),
+                        tint = MaterialTheme.colorScheme.onPrimaryContainer,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(30.dp)
+                    )
+                },
+                onClick = { onSelected(i) },
+                selected = selected == i
+            )
         }
     }
 }
--- Ademanos-Android-App/app/src/main/java/com/example/ademanos_android_app/MainActivity.kt
+++ Ademanos-Android-App/app/src/main/java/com/example/ademanos_android_app/MainActivity.kt
@@ -5,35 +5,71 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ademanos_android_app.levelTab.LevelManager
+import com.example.ademanos_android_app.loginScreen.LoginScreen
 import com.example.ademanos_android_app.ui.theme.AdemanosAndroidAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AdemanosAndroidAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    val dictionaryTabScreen= BottomNavScreen("Dictionary Tab",R.drawable.book_solid
-                    ) {  }
+            AdemanosApp()
+        }
+    }
+}
 
-                    val levelTabScreen= BottomNavScreen("Level Tab",R.drawable.gamepad_solid
-                    ) { LevelManager(TEST_QUIZ) }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AdemanosApp(appViewModel: AppViewModel = viewModel()) {
+    AdemanosAndroidAppTheme {
+        val dictionaryTabScreen = BottomNavScreen(
+            "Dictionary Tab", R.drawable.book_solid
+        ) { }
 
-                    val profileTabScreen= BottomNavScreen("Profile Tab",R.drawable.user_solid
-                    ) { }
+        val levelTabScreen = BottomNavScreen(
+            "Level Tab", R.drawable.gamepad_solid
+        ) { LevelManager(TEST_QUIZ) }
 
-                    val screens = listOf(dictionaryTabScreen,levelTabScreen,profileTabScreen)
-                    BottomNavigation(screens)
+        val profileTabScreen = BottomNavScreen(
+            "Profile Tab", R.drawable.user_solid
+        ) { LoginScreen() }
+        val screens = listOf(dictionaryTabScreen, levelTabScreen, profileTabScreen)
+        Scaffold(
+            bottomBar = {
+                BottomNavigation(
+                    screens = screens,
+                    onSelected = { i -> appViewModel.selectScreen(i) },
+                    selected = appViewModel.selectedScreen
+                )
+            },
+            snackbarHost = {
+                SnackbarHost(hostState = appViewModel.snackbarHostState) { data ->
+                    Snackbar(
+                        modifier = Modifier.padding(12.dp),
+                        shape = CutCornerShape(4.dp),
+                        containerColor = MaterialTheme.colorScheme.error,
+                        action = {
+                            TextButton(
+                                onClick = { data.dismiss() },
+                                colors = ButtonDefaults
+                                    .textButtonColors(contentColor = MaterialTheme.colorScheme.onError)
+                            ) {
+                                Text("Ok")
+                            }
+                        }
+                    ) {
+                        Text(data.visuals.message)
+                    }
                 }
             }
+        ) { padding ->
+            screens[appViewModel.selectedScreen].component(Modifier.padding(padding))
         }
     }
 }
@@ -41,5 +77,5 @@ class MainActivity : ComponentActivity() {
 data class BottomNavScreen(
     val label: String,
     @DrawableRes val icon: Int,
-    val component: @Composable () -> Unit
+    val component: @Composable (Modifier) -> Unit
 )
