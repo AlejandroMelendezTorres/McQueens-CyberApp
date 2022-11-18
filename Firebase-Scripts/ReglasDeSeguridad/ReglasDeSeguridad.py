7e953d5cf4077d4d36368b2b6edd9b0a3b31ff4f
Miguel Arriaga <marriagav3@gmail.com>
2022-11-17 20:27:41 -0600

    Update ReglasDeSeguridad.py

--- Firebase-Scripts/ReglasDeSeguridad/ReglasDeSeguridad.py
+++ Firebase-Scripts/ReglasDeSeguridad/ReglasDeSeguridad.py
@@ -8,11 +8,18 @@ service cloud.firestore {
     match /users/{user} {
     	allow read: if request.auth.uid == user;
       allow write: if isAdmin(request);
+      allow update: if request.auth.uid == user && (request.resource.data.diff(resource.data).affectedKeys()
+        .hasOnly(['consultedWords', 'completedLevels']));
       
       match /events/{event} {
       	allow read: if isAdmin(request);
         allow write: if request.auth.uid == user || isAdmin(request);
       }
+    }
+    
+    match /events/{event} {
+      	allow read: if isAdmin(request);
+        allow write: if request.auth.uid == request.resource.data.uid || isAdmin(request);
     }
     
     match /categories/{category} {
